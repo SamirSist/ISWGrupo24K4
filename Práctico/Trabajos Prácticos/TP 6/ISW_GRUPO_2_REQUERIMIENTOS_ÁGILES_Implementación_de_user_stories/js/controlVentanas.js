06477a8d47bf13a3d34bda60f56911a910579480
Gastón Barrionuevo <62447533+gastoncba@users.noreply.github.com>
2021-05-09 17:07:28 -0300

    Se corrige TP 6

---
+++ Práctico/Trabajos Prácticos/TP 6/ISW_GRUPO_2_REQUERIMIENTOS_ÁGILES_Implementación_de_user_stories/js/controlVentanas.js
@@ -0,0 +1,53 @@
+
+function mostrarDetalle(){
+    $(".detallePedido").toggleClass("d-none");
+};
+
+function mostrarReloj(){
+    $(".timePicker").removeClass("d-none");
+}
+function ocultarReloj(){
+    $(".timePicker").addClass("d-none");
+}
+
+function mostrarEfectivo() {
+    $(".efectivoForm").removeClass("d-none");
+    $(".datosTarjeta").addClass("d-none");
+    $(".listadoTarjetas").addClass("d-none");
+    
+}
+function mostrarTarjeta() {
+    $(".efectivoForm").addClass("d-none");
+    $(".datosTarjeta").removeClass("d-none");
+    $(".datosTarjeta").addClass("d-block");
+    $(".listadoTarjetas").removeClass("d-none");
+    
+}
+
+function mostrarResumen() {
+    $(".resumenPedido").removeClass("d-none");
+    $(".resumenPedido").addClass("d-block");
+}
+
+function ocultarResumen() {
+    $(".resumenPedido").addClass("d-none");
+    $(".resumenPedido").removeClass("d-block");
+}
+function ocultarTarjeta() {
+    $(".datosTarjeta").addClass("d-none");
+    $(".listadoTarjetas").addClass("d-none");
+    $(".datosTarjeta").removeClass("d-block");
+    $(".efectivoForm").removeClass("d-none");
+    $('#efectivo').prop('checked', true);
+}
+
+function agregarTarjeta(){
+    $(".datosTarjeta").addClass("d-none");
+    $(".datosTarjeta").removeClass("d-block");
+    $("#efectivo").removeAttr('checked');
+}
+
+$("#detalleBtn").click(function(){
+    $(".detallePedido").show();
+  });
+
---
+++ Práctico/Trabajos Prácticos/TP 6/ISW_GRUPO_2_REQUERIMIENTOS_ÁGILES_Implementación_de_user_stories/js/gestorPedido.js
@@ -0,0 +1,146 @@
+/**
+ * Obtenemos los elementos del HTML
+ */
+
+let btnPedido = document.getElementById("colorBoton");
+
+let datosEntrega = document.getElementsByClassName("campo");
+
+let opEfectivo = document.getElementById("efectivo");
+
+btnPedido.addEventListener("click", (e)=>{
+    
+    /**
+     * aca hay que validar si todos los campos  fueron correctos
+     * primero validamo si el total no es cero , ya que si es cero , el carrito esta vacio
+     */
+
+    let valorTotalCarro = parseFloat(document.getElementById("total").innerText);
+    
+    console.log(valorTotalCarro);
+    //es 100 por el costo de envio
+    if(valorTotalCarro === 100) {
+        alert('Su carrito se encuentra vacio!!');
+        return;
+    }
+    
+    for (let i = 0; i < datosEntrega.length; i++) {
+        let dato = datosEntrega[i];
+        //preguntamos si hay algo en el campo
+        if(!hayAlgo(dato.value)) {
+            //si no hay algo  ,prguntamos si esta marcado el efectio ya que debe analizarlo en caso de estarlo
+            if(dato.id === "montoEfectivo" && opEfectivo.checked) {
+                alert('Usted no ingresó ' + campo(dato.id)); 
+                return;
+            }
+            else if(dato.id === "montoEfectivo"){
+                continue;
+            }
+            alert('Usted no ingresó ' + campo(dato.id));
+            return;
+        }
+        
+        
+    }
+
+    /**
+     * si esta todo ok debemos enviar un mensaje al usuario con su detalle y costo de pedido
+     */
+     mostrarResumen()
+     var textoDetalle = ""
+     let ciudad = document.getElementById("selectCiudad").value;
+     let nombreCalle = document.getElementById("nombreCalle").value;
+     let deptoNum = document.getElementById("deptoNum").value;
+     let loAntesPosible = document.getElementById("loAntesPosible");
+     let programarOpt = document.getElementById("programarOpt");
+     let datetime = document.getElementById("fecha-envio").value;
+     let efectivo = document.getElementById("efectivo");
+     let tarjetaCredito = document.getElementById("tarjetaCredito");
+     let montoEfectivo= document.getElementById("montoEfectivo").value;
+     let detallePedido = document.getElementById("detalleResPedido");
+     textoDetalle += "Dirección: "
+     textoDetalle += queCiudad(ciudad);
+     textoDetalle += ' -' + nombreCalle;
+     textoDetalle += ' -' + deptoNum;
+     if(loAntesPosible.checked) {
+        textoDetalle += '<br> -Lo antes posible';
+     }
+     if(programarOpt.checked) {
+        textoDetalle += '<br> -Programado para: ';
+        textoDetalle += datetime;
+     }
+
+    textoDetalle += '<br> -Total: ';
+    textoDetalle += valorTotalCarro;
+    
+    if(efectivo.checked) {
+        textoDetalle += '<br> -Pago en efectivo: ';
+        textoDetalle += montoEfectivo;
+     }
+
+     if(tarjetaCredito.checked) {
+        textoDetalle += '<br> -Pago con tarjeta Visa';
+     }
+
+     detallePedido.innerHTML = textoDetalle;   
+    
+})
+
+/**funcion que valida si hay algo en los campos */
+function hayAlgo(valor) {
+    return valor !== '';
+}
+
+
+function campo(id) {
+    if(id === "selectCiudad"){
+        return "cuidad";
+    }
+
+    else if(id === "nombreCalle") {
+        return "nombre de calle";
+    }
+
+    else if(id === "deptoNum") {
+        return "nombre de departamento";
+    }
+    else if(id === "referenciaDir") {
+        return "refencia";
+    }
+    else if(id === "montoEfectivo") {
+        return "monto a pagar";
+    }
+    
+    else {
+        return "un dato obligatorio";
+    }
+}
+
+function queCiudad(num) {
+    if(num == 1){
+        return "Córdoba";
+    }
+
+    else if(num == 2) {
+        return "Rio Cuarto";
+    }
+
+    else if(num == 3) {
+        return "Villa María";
+    }
+    
+    else {
+        return "No es una ciudad válida.";
+    }
+}
+
+
+
+function confirmarPedido(){
+    let confirmacionPedido = document.getElementById("overContent2");
+    var textoHTML = "";
+    textoHTML = "<div class='row'><div class='col-7 h3'> Pedido Registrado Con Éxito </div> "
+    textoHTML += "<div class='col-3 h1'> <i id='logoChecked' class='fa fa-check-circle '></i> </div> </div>"
+    confirmacionPedido.innerHTML = textoHTML;
+
+}
